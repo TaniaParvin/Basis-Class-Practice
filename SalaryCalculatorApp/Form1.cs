57b32e0adba0046d70f7cdbc4772ba70c3e5b435
TaniaParvin <tania.iubd@gmail.com>
2014-09-27 14:11:04 +0600

    file uploaded

---
+++ SalaryCalculatorApp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        SalaryCalculator aSalaryCalculator=new SalaryCalculator();
+        public string name;
+        public double basicAmount;
+        public double houseRent;
+        public double medicalAllowance;
+        public double salary;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+           name = nameTextBox.Text;
+            basicAmount = Convert.ToDouble(amountTextBox.Text);
+            houseRent = Convert.ToDouble(houseRentTextBox.Text);
+            medicalAllowance = Convert.ToDouble(medicalTextBox.Text);
+            salary = Convert.ToDouble(aSalaryCalculator.calculateSalary(basicAmount, houseRent, medicalAllowance));
+            string msg = name + "  your salary is " + Convert.ToString(salary);
+
+           // MessageBox.Show(Convert.ToString(salary));
+            MessageBox.Show(msg);
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorApp
+{
+    class SalaryCalculator
+    {
+      
+        public double salary;
+        
+        public double calculateSalary(double basicAmount, double houseRent, double medicalAllowance)
+        {
+           return salary = basicAmount + (basicAmount*houseRent)/100 + (basicAmount*medicalAllowance)/100;
+        }
+    }
+}
