0c014b7d03282e41a83a92498ceed425fdf6d2eb
TaniaParvin <tania.iubd@gmail.com>
2014-10-27 13:05:48 +0600

    commit

---
+++ EscapeChar/EscapeChar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EscapeChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string greetings = "Hello everyone.\nWelcome to OOP class";
+           // Console.WriteLine(greetings);
+
+            //string address = @"C:\Software\Books\Beginning";
+            string address = @"""Hello everyone"" ,said the the trainer.";
+
+            Console.WriteLine(address);
+            Console.ReadKey(); 
+        }
+    }
+}
