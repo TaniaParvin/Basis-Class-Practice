b4ecbbebf2d223de4fa86346b41cdb3efcc7352a
TaniaParvin <tania.iubd@gmail.com>
2014-10-30 12:14:01 +0600

    exception

---
+++ ExceptionApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ExceptionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noOfTicketsTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // totalButton
+            // 
+            this.totalButton.Location = new System.Drawing.Point(178, 127);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(75, 23);
+            this.totalButton.TabIndex = 0;
+            this.totalButton.Text = "Total Price";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No of Tickets";
+            // 
+            // noOfTicketsTextBox
+            // 
+            this.noOfTicketsTextBox.Location = new System.Drawing.Point(121, 54);
+            this.noOfTicketsTextBox.Name = "noOfTicketsTextBox";
+            this.noOfTicketsTextBox.Size = new System.Drawing.Size(158, 20);
+            this.noOfTicketsTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "One Ticket 100 Taka";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.noOfTicketsTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox noOfTicketsTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExceptionApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExceptionApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                byte noOfTickets = Convert.ToByte(noOfTicketsTextBox.Text);
+                double total = noOfTickets*100;
+                MessageBox.Show(noOfTickets.ToString());
+
+            }
+            catch (OverflowException overflowException)
+            {
+
+                MessageBox.Show("Please enter number less than 255");
+            }
+            catch (FormatException formatException)
+            {
+                MessageBox.Show("Please  Enter number only");
+            }
+
+        }
+    }
+}
